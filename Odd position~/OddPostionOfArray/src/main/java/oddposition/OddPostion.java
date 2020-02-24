d8f34ade28a9de77a4f91cf427b47411224f3d65
Dinesh Nanda <dannynanda76@gmail.com>
2020-02-24 11:30:02 -0500

    print elements at odd position of an array

---
+++ Odd position~/OddPostionOfArray/src/main/java/oddposition/OddPostion.java
@@ -0,0 +1,38 @@
+package oddposition;
+
+/**
+ *
+ * @author Dinesh Nanda
+ */
+
+import java.util.*;
+
+public class OddPostion {
+
+    public static void main(String[] args) {
+        
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter the size of array: ");
+        int size = s.nextInt();
+        
+        int arr[];
+        arr = new int[size];
+        
+        for(int i = 0; i < size; i++){
+            arr[i] = s.nextInt();
+        }
+        System.out.println("Your entered array is: ");
+        for(int i = 0; i < size; i++){
+            System.out.print(arr[i]+" ");
+        }
+        System.out.println();
+        System.out.println("Elements at odd position of array is:");
+        
+        for(int i = 0; i < size; i++){
+            if(i%2 != 0){
+                System.out.print(arr[i]+" ");           
+            }
+        }
+    }
+    
+}
